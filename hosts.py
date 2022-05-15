e1ca406850144d28cccb3b5e8309f3ca1f87ff23
Wenhan Deng <wenhandeng@WenhandeMac-mini.local>
2022-05-15 14:26:18 -0500

    first

--- hosts.py
+++ hosts.py
@@ -6,18 +6,49 @@ def findhosts(year):
     # load english language model
     nlp = spacy.load('en_core_web_sm')
     pattern = re.compile(r'host', re.I)
+    name_pattern = re.compile(r'[A-Z][a-z]* [A-Z][a-z]*')
+    year_pattern = re.compile(r'\d{4}')
     result = handle_Data(year)
+    
     hosts = {}
+    total = 0
     for out in result:
-        if re.search(pattern, out):
-            tok = nlp(out)
-            for names in tok.ents: 
-                if names.label_ == 'PERSON':
-                    if names.text in hosts:
-                        hosts[names.text] += 1
+        temp = out.lower()
+        if 'future' in temp or 'wish' in temp or 'will' in temp or "next year" in temp or "petition" in temp:
+            continue
+        if (re.search(pattern, out)):
+            numbers = year_pattern.findall(out)
+            check = True
+            for number in numbers:
+                if number != year:
+                    check = False
+            if check:
+                navie = name_pattern.findall(out)
+                for names in navie:
+                    if 'Golden Globe' in names:
+                        continue
+                    na = names.lower()
+                    if na in hosts:
+                        hosts[na] += 1
                     else:
-                        hosts[names.text] = 1
+                        hosts[na] = 1
+    hosts = sorted(hosts.items(), key = lambda kv:kv[1], reverse= True)
     print(hosts)
+    hosts = hosts[:5]
+    hosts = list(map(list, hosts))
+    """
+    for x in range(5):
+        if ' ' not in hosts[x][0]:
+            for y in range(5):
+                if x != y and hosts[x][0] in hosts[y][0]:
+                    hosts[y][1] += hosts[x][1]
+                    hosts[x][1] = 0
+    hosts = sorted(hosts, key = lambda kv:kv[1], reverse= True)
+    hosts = hosts[:2]
+    print(hosts)
+    """
 
-findhosts(2013)
+
+findhosts(2015)
+
 
