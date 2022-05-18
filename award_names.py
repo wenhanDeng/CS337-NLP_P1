14c894a97f4247c893d8eff19eed5fea5d075bb7
Wenhan Deng <wenhandeng@WenhandeMac-mini.local>
2022-05-18 13:00:35 -0500

    latest

--- award_names.py
+++ award_names.py
@@ -1,12 +1,13 @@
 
+from ast import Num
 from lib2to3.pgen2 import token
 from data import handle_Data
 import spacy
 import re
 
-stop_word_exclusion = ['by', 'an', 'a', 'or', 'in', 'made', 'for']
-punt_exclusion = ['-', ',']
-
+stop_word_exclusion = ['by', 'an', 'a', 'or', 'in', 'made']
+punt_exclusion = [',']
+a_list = []
 # nlp = spacy.load('en_core_web_sm')
 # a = nlp("for")
 # for tok in a:
@@ -14,8 +15,13 @@ punt_exclusion = ['-', ',']
 
 def find_award_names(year):
     # load english language model
+    if year == 2013 or year == 2015:
+        num = 28
+    else:
+        num = 28
     nlp = spacy.load('en_core_web_sm')
     nlp.Defaults.stop_words |= {'goes', 'golden'}
+    added_stop = {'-', 'win', 'won'}
     pattern = re.compile(r'best', re.I)
     result = handle_Data(year)
     awards_dict = {}
@@ -30,15 +36,7 @@ def find_award_names(year):
             for tok in tem:               
                 if (tok.is_stop and tok.text not in stop_word_exclusion) or (tok.is_punct and tok.text not in punt_exclusion):
                     break
-                if '-' in res and tok.text == '-':
-                    break
-                if tok.text == 'tv':
-                    res.append('television')
-                    continue
-                if tok.ent_type_ == 'PERSON':
-                    if len(res) != 0 and  res[-1] == '-':
-                        res.pop()
-                    break
+
                 res.append(tok.text)
             
             if len(res) <= 3:
@@ -48,9 +46,11 @@ def find_award_names(year):
                 awards_dict[res] += 1
             else:
                 awards_dict[res] = 1
-    for i in awards_dict:
-        if awards_dict[i] >= 10:
-            print(i, awards_dict[i])
- 
+    awards_dict = sorted(awards_dict.items(), key = lambda kv:kv[1], reverse= True)    
+    if len(awards_dict) < num:
+        num = len(awards_dict)
+    for i in range(num):
+        a_list.append(awards_dict[i])
+    return a_list
   
 find_award_names(2013)
